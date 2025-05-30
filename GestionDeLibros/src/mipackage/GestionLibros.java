c26fbe3f364a3666a25003a6b4f55f8e14988fd5
iván <romuvan13@gmail.com>
2025-05-30 10:50:23 +0200

    Subiendo javadoc

--- GestionDeLibros/src/mipackage/GestionLibros.java
+++ GestionDeLibros/src/mipackage/GestionLibros.java
@@ -6,6 +6,10 @@ public class GestionLibros {
     private static String[] libros = new String[MAX_LIBROS];
     private static int contador = 0;
 
+    /**
+     * Método main
+     * @param args
+     */
     public static void main(String[] args) {
         añadirLibro("Don Quijote");
         añadirLibro("Cien años de soledad");
@@ -21,6 +25,12 @@ public class GestionLibros {
         }
     }
 
+    /**
+     * Método que pide un título de un libro y lo añade
+     * a un array de libros
+     * @param titulo String que sea el nombre del libro a añadir
+     * @return true si se pudo añadir el libro false si no
+     */
     public static boolean añadirLibro(String titulo) {
         if (contador < MAX_LIBROS) {
             libros[contador] = titulo;
@@ -30,6 +40,13 @@ public class GestionLibros {
         return false;
     }
 
+    /**
+     * Método al que se le pasa el título de un libro
+     * por parámetro y te devuelve la posición en la 
+     * que se encuentra dentro del array.
+     * @param titulo String que sea el nombre del libro a añadir
+     * @return La posición donde se encuentra en libro
+     */
     public static int buscarLibro(String titulo) {
         for (int i = 0; i < contador; i++) {
             if (libros[i].equalsIgnoreCase(titulo)) {
@@ -39,10 +56,20 @@ public class GestionLibros {
         return -1;
     }
 
+    /**
+     * Cuenta los libros que hay
+     * @return La cantidad de libros
+     */
     public static int contarLibros() {
         return contador;
     }
 
+    /**
+     * Método al que se le pasa un índice por parámetro 
+     * para mostrar el libro que se encuentre en esa posición
+     * @param indice Supuesta posición del libro
+     * @return El libro buscado si se encuentra o el texto "Índice fuera de rango" si el índice pasado por parámetro esta fuera de rango.
+     */
     public static String mostrarLibro(int indice) {
         if (indice >= 0 && indice < contador) {
             return libros[indice];
